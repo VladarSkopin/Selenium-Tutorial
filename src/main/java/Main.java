a5c52616511c7b0ecae10c8d8cec1ccec0e4ac6d
VladarSkopin <grudagor@gmail.com>
2022-11-11 17:12:40 +0300

    My very first Selenium script

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
-        
+        System.setProperty("webdriver.chrome.driver","D:\\SeleniumProjectsQA\\chromedriver.exe");
+
+        //ChromeDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
     }
 }
