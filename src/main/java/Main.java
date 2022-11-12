1e103b695621b3895c2ac3444f28905a55316703
VladarSkopin <grudagor@gmail.com>
2022-11-12 09:37:09 +0300

    added ExpectedConditions - implicit and explicit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +15,18 @@ public class Main {
         //ChromeDriver driver = new ChromeDriver();
         WebDriver driver = new ChromeDriver();
 
+        // НЕЯВНЫЕ ОЖИДАНИЯ - ожидания по умолчанию (быстрее, чем явные)
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // DEPRECATED
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Selenium 4 style
+
+        // ЯВНЫЕ ОЖИДАНИЯ - ожидания по конкретному элементу
+        // to wait for an element to appear within 10 seconds:
+        WebElement element = (new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("123"))));
+
+
         driver.get("https://google.com");
+
+
     }
 }
