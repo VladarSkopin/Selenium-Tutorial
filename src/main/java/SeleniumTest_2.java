39f2555ccf9e6cc811853498b7de6e515d5f60dc
VladarSkopin <grudagor@gmail.com>
2022-11-14 11:38:20 +0300

    getText test

---
+++ src/main/java/SeleniumTest_2.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest_2 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:\\SeleniumProjectsQA\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.avito.ru/");
+
+        WebElement elementLink = driver.findElement(By.xpath("//a[text()='Работа']"));
+        System.out.println("text = " + elementLink.getAttribute("text"));
+        System.out.println("class = " + elementLink.getAttribute("class"));
+        System.out.println("clientWidth = " + elementLink.getAttribute("clientWidth"));
+
+
+        // getText -> to get the text exactly the way it's shown to the user
+        String elementText = elementLink.getText();
+        System.out.println("element text = " + elementText);
+
+
+    }
+}
